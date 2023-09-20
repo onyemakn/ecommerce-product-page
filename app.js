09f14e22f43f85f4b57758d22e10371c0d97aab8
Kizito Onyema <onyemakizito@outlook.com>
2023-09-20 17:12:13 +0200

    updated desiign

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 
 const previewClick = document.querySelectorAll('.main-image')
-previewClick.forEach((e) => e.addEventListener("click", getClass))
+const productImage = document.getElementById('main-image')
+const mainImageSrc = productImage.getAttribute('src')
 
-function getClass() {
-  const cls = this.getAttribute('src')
-    console.log(cls)
-
+for (let i = 0; i < previewClick.length; i++) {
+  const thumbnailSrc = previewClick[i]
+  if (mainImageSrc === thumbnailSrc.getAttribute('src')) {
+    thumbnailSrc.style.opacity = "0.3"
+  }
 }
+
+previewClick.forEach((e) => e.addEventListener("click", getClass))
+function getClass() {
+  for (let i = 0; i < previewClick.length; i++) {
+    const thumbnailSrc = previewClick[i]
+    if (thumbnailSrc.style.opacity === "0.3") {
+      thumbnailSrc.style.opacity = "1"
+    }
+  }
+  const image_thumbnail = this.getAttribute('src')
+  productImage.setAttribute('src', image_thumbnail)
+  this.style.opacity = "0.3"
+}
+
+
 
